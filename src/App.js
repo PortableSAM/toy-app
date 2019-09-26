9b74d34b56050f59b83370e01619a0cf252b1412
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-09-26 17:08:07 +0900

    실습

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Potato from './potato';
 
 function App() {
-  return <div > < h1 > Hell React!! < /h1></div >
-}
+  return ( < div > < h1 > Hell React!! < /h1>  <
+    Potato / >
+    <
+    /div >);
+  }
 
-export default App;
+  export default App;
